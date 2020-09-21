3164694d10db5b8407f8b5c8a153e08ab45ee516
Roshendz <zoysa3@me.com>
2020-09-22 01:19:40 +0200

    Add claases commit

---
+++ app/src/main/java/com/roshendilan/topquiz/model/QuestionBank.java
@@ -0,0 +1,31 @@
+package com.roshendilan.topquiz.model;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Roshen Dilan on 2020-09-21.
+ */
+public class QuestionBank {
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+    public QuestionBank(List<Question> questionList) {
+        mQuestionList = questionList;
+
+        // Shuffle the question list
+        Collections.shuffle(mQuestionList);
+
+        mNextQuestionIndex = 0;
+    }
+
+    public Question getQuestion() {
+        // Ensure we loop over the questions
+        if (mNextQuestionIndex == mQuestionList.size()) {
+            mNextQuestionIndex = 0;
+        }
+
+        // Please note the post-incrementation
+        return mQuestionList.get(mNextQuestionIndex++);
+    }
+}
---
+++ app/src/main/java/com/roshendilan/topquiz/model/User.java
@@ -0,0 +1,16 @@
+package com.roshendilan.topquiz.model;
+
+/**
+ * Created by Roshen Dilan on 2020-09-21.
+ */
+public class User {
+    private String mFirstName;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+}
